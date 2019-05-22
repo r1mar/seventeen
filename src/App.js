93aac68716f286ae6fb1a4972cbda80a3c04414e
Richard Martens <richard.martens@outlook.com>
2019-05-22 21:35:56 +0200

    migration

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="Portraitstudio" description="Wir malen ein Porträt von Ihrem Foto" root="/" />
+      <section></section>
+    </div>
+  );
+}
+
+export default App;
